3f877dda66b07a7d825bba2976f1ff84712618ea
isaacduarte <isaacramonc@gmail.com>
2020-01-20 06:54:01 -0300

    primeira e segunda aula

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+return response.json({message:'hello omnistack'});
+
+});
+
+app.listen(3333);
